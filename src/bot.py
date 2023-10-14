7e3a0add325b329f1d240e042fb721b8e2f719f4
Nikita Davidchuk <bizy18588@gmail.com>
2023-10-14 16:12:26 +0300

    Add optional argument parser

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,5 @@
+import re
+
 import telebot
 from telebot.types import Message
 
@@ -5,6 +7,37 @@ from dependencies import TELEGRAM_BOT_API
 from kandinsky import generate_img
 
 bot = telebot.TeleBot(TELEGRAM_BOT_API)
+
+command_arguments = {
+    "negative": r"(-n|—n|--neg|--negative|—neg|—negative)",
+    "seed": r"(-s|--seed|—seed|—s)",
+}
+
+
+def clear_telegram_message(plain_text: str, arg_dict: dict[str, str]):
+    main_message = plain_text
+
+    for arg_field in arg_dict.values():
+        arg_pattern = f"{arg_field}\s+([^\\s]+(\s+[^\\s]+)*)"
+        main_message = re.sub(arg_pattern, "", main_message)
+
+    # Remove leading/trailing whitespace from the main message
+    main_message = main_message.strip()
+
+    return main_message
+
+
+def parse_telegram_args(plain_text: str, arg_dict: dict[str, str]):
+    args = {}
+
+    for arg_name, arg_field in arg_dict.items():
+        arg_pattern = f"{arg_field}\s+([^\\s]+(\s+[^\\s]+)*)"
+        matches = re.findall(arg_pattern, plain_text)
+        if matches:
+            args[arg_name] = matches[-1][1]
+            args[arg_name] = clear_telegram_message(args[arg_name], arg_dict)
+
+    return args
 
 
 # Handle '/start' and '/help'
@@ -32,10 +65,13 @@ def func(message: Message):
     :param message: telebot.types.Message
     :return:
     """
+    main_message = clear_telegram_message(message.text, command_arguments)
+    args = parse_telegram_args(message.text, command_arguments)
+
     new_message = bot.send_message(
         chat_id=message.chat.id, text="Идет генерация изображения..."
     )
-    generated_image = generate_img(message.text)
+    generated_image = generate_img(text_prompt=main_message, **args)
 
     bot.delete_message(chat_id=new_message.chat.id, message_id=new_message.message_id)
     bot.send_photo(chat_id=message.chat.id, photo=generated_image[0])
--- src/kandinsky.py
+++ src/kandinsky.py
@@ -1,16 +1,24 @@
 import replicate
-from dependencies import REPLICATE_API
 
 
-def generate_img(text_prompt: str):
+def generate_img(text_prompt: str, negative: str = None, seed: str = None):
+    arguments = {"prompt": text_prompt}
+
+    if negative:
+        arguments["negative_prompt"] = negative
+
+    if seed:
+        if seed.isdigit():
+            arguments["seed"] = int(seed)
+
     output = replicate.run(
         "ai-forever/kandinsky-2.2:ea1addaab376f4dc227f5368bbd8eff901820fd1cc14ed8cad63b29249e9d463",
-        input={"prompt": text_prompt}
+        input=arguments,
     )
     print(output)
 
     return output
 
 
-if __name__ == '__main__':
-    generate_img('a black sheep with white god ray and black background')
+if __name__ == "__main__":
+    generate_img("a black sheep with white god ray and black background")
