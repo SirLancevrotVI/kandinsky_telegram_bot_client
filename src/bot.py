e966808443968c8fa41bc0b7ec6096b0eb62fcdf
Nikita Davidchuk <bizy18588@gmail.com>
2023-10-14 14:16:59 +0300

    Optimize imports; reformat code style

--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,8 @@
 import telebot
 from telebot.types import Message
-from kandinsky import generate_img
-from telebot import types
+
 from dependencies import TELEGRAM_BOT_API
-from telebot import types
+from kandinsky import generate_img
 
 bot = telebot.TeleBot(TELEGRAM_BOT_API)
 
@@ -11,9 +10,15 @@ bot = telebot.TeleBot(TELEGRAM_BOT_API)
 # Handle '/start' and '/help'
 @bot.message_handler(commands=["help", "start"])
 def send_welcome(message):
+    """
+    Send welcome message
+    :param message: telebot.types.Message
+    :return:
+    """
     bot.reply_to(
         message,
-        text='Для того, чтобы сгенерировать изображение, отправьте в следующем сообщении ключевые слова через запятую.'.format(
+        text="Для того, чтобы сгенерировать изображение,"
+        " отправьте в следующем сообщении ключевые слова через запятую.".format(
             message.from_user
         ),
     )
@@ -27,12 +32,14 @@ def func(message: Message):
     :param message: telebot.types.Message
     :return:
     """
-    new_message = bot.send_message(chat_id=message.chat.id, text='Идет генерация изображения...')
-    text_prompt: str = message.text
-    generated_image = generate_img(text_prompt)
+    new_message = bot.send_message(
+        chat_id=message.chat.id, text="Идет генерация изображения..."
+    )
+    generated_image = generate_img(message.text)
+
     bot.delete_message(chat_id=new_message.chat.id, message_id=new_message.message_id)
     bot.send_photo(chat_id=message.chat.id, photo=generated_image[0])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot.infinity_polling(timeout=99999, skip_pending=True)
